d00f8a0388685402826a7551050fe7e10cca6246
naizal Edy <pritiadhikari@gmail.com>
2018-10-18 23:23:36 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -4,16 +4,15 @@ const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 let index = 0;
   function init() {
   
-  document.addEventListener("keydown", function(e){
+  let index = 0;
+  
+  document.body.addEventListener('keydown', (e) => {
     const key = e.key;
-     if(key === codes[index]){
-      index++;
-       if(index === codes.length){
-        alert("Yay! You did it!");
-        index = 0;
-      }
-    }else{
+    index = (codes[index] === key) ? ++index : 0
+    
+    if (index === codes.length) {
+      window.alert ('Well Done!');
       index = 0;
     }
-}, false);
-}	
+  });
+} 
