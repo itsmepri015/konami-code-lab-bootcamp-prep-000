850c32487faed4aaf750ea995fd0e7f0fea4d443
naizal Edy <pritiadhikari@gmail.com>
2018-10-19 00:56:21 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -1,10 +1,11 @@
 const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 	
 
-let index = 0;
+
   function init() {
   
-  document.addEventListener('keydown', function(e){
+  let index = 0;
+  document.body.addEventListener('keydown', function(e){
     const key = e.key;
      if(key === code[index]){
       index++;
