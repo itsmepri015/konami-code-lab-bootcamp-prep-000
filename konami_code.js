afd2480a9a26d7d981c35e20a2742bf8c94f5161
naizal Edy <pritiadhikari@gmail.com>
2018-10-19 00:53:18 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -8,7 +8,7 @@ let index = 0;
     const key = e.key;
      if(key === codes[index]){
       index++;
-       if(index === codes.length){
+       if(index === 9){
         alert("Yay! You did it!");
         index = 0;
       }
