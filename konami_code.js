dd27144d7b0ed73db440fe2b3a52deb4e86f8d2b
naizal Edy <pritiadhikari@gmail.com>
2018-10-19 01:28:22 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -3,7 +3,7 @@
 
 const code = ['ArrowUp', 'ArrowUp', 'ArrowDown', 'ArrowDown', 'ArrowLeft', 'ArrowRight', 'ArrowLeft', 'ArrowRight', 'b', 'a'];
 	function init() {
-  // Write your JavaScript code inside the init() function	  // Write your JavaScript code inside the init() function
+ 
    let codeCount = 0;
   
   document.body.addEventListener('keydown', function(e) {
