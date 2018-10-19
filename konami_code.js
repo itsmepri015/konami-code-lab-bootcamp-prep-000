9a477efb043018ec41fcd1ca5b1dcfa882f7e4ee
naizal Edy <pritiadhikari@gmail.com>
2018-10-19 01:00:37 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -14,7 +14,7 @@ const Code = [
 
 function init() {
   let step = 0;
-  document.body.addEventListener(`keydown`, function(press) {
+  document.addEventListener(`keydown`, function(press) {
     const button = press.key;
     if (button === Code[step]) {
       step++;
